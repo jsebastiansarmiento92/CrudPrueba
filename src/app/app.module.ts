d24dd4e66ab5059870268647017a623efa5a1cf9
Sebastian <jsebastiansarmiento92@gmail.com>
2019-12-23 22:56:57 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms'
+import {BrowserAnimationsModule  } from "@angular/platform-browser/animations";
+//import { ToastrModule } from "ngx-toastr";
+//firebase
+import {AngularFireDatabaseModule} from 'angularfire2/database'
+import {AngularFireModule} from 'angularfire2'
+import{AngularFireStorageModule}from 'angularfire2/storage';
 
 import { AppComponent } from './app.component';
 
