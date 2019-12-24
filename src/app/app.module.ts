78f43207b185152bbec4e71a68d8b59f0d0603d9
Sebastian <jsebastiansarmiento92@gmail.com>
2019-12-23 23:06:20 -0500

    generador de modulo de rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,16 +8,47 @@ import {AngularFireDatabaseModule} from 'angularfire2/database'
 import {AngularFireModule} from 'angularfire2'
 import{AngularFireStorageModule}from 'angularfire2/storage';
 
+//componentes
 import { AppComponent } from './app.component';
+import {environment}from '../environments/environment';
+import { ProductsComponent } from './components/products/products.component';
+import { ListProductsComponent } from './components/Products/list-products/list-products.component';
+import { ProductComponent } from './components/Products/product/product.component';
+
+
+//services
+import{ProductService}from './services/product.service';
+import { CountriesComponent } from './components/countries/countries.component'
+import {HttpClientModule}from '@angular/common/http'
+
+import{AuthService}from './services/auth.service';
+import { AppRoutingModule } from './app-routing.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    ListProductsComponent,
+    ProductComponent,
+    CountriesComponent
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule ,
+    AngularFireStorageModule,
+    FormsModule,
+    HttpClientModule,
+    //ToastrModule.forRoot(),
+    BrowserAnimationsModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    ProductService,
+    AuthService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyC0Qa9-1aTr8ditoI4fJzoWlU5VIq2zx6U",
+    authDomain: "pruebaangularcrud-56f88.firebaseapp.com",
+    databaseURL: "https://pruebaangularcrud-56f88.firebaseio.com",
+    projectId: "pruebaangularcrud-56f88",
+    storageBucket: "pruebaangularcrud-56f88.appspot.com",
+    messagingSenderId: "55879453702",
+    appId: "1:55879453702:web:6faa0c16a734de389cc787",
+    measurementId: "G-SS3RFBVR6Q"
+  }
+
 };
 
 /*
